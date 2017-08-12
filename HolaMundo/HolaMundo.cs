e43897393d68e98dcae77280a5b8483012b43ec0
Ricardo R Quintero Meza <iscrquinter@outlook.com>
2017-08-12 10:25:54 -0600

    Saludo General ...

---
+++ HolaMundo/HolaMundo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    class HolaMundo
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
